327a6dd6f79669a5e5f408599bc97758af21460c
Max Upravitelev <max@upravitelev.de>
2020-07-15 08:50:29 +0200

    create modules for ListID handling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Item from "./components/Item";
 import AddItemForm from "./components/AddItemForm";
+import ShowListID from "./components/ShowListID";
+import ShowNewListID from "./components/ShowNewListID";
 import axios from "axios";
 import logo from "./img/logo.png";
 
@@ -153,71 +155,6 @@ const App = () => {
       <br />
     </div>
   );
-};
-
-const ShowListID = ({ listId, newListId }) => {
-  const handleShareList = () => {
-    // let copyListURL = document.getElementById("copyListURL");
-    let copyListURL =
-      "http://shopping-assistent.herokuapp.com/?listId:" + listId;
-
-    let tmp = document.createElement("textarea");
-    tmp.value = copyListURL;
-    tmp.style.height = "0";
-    tmp.style.overflow = "hidden";
-    tmp.style.position = "fixed";
-    document.body.appendChild(tmp);
-    tmp.focus();
-
-    tmp.select();
-    tmp.setSelectionRange(0, 99999);
-
-    document.execCommand("copy");
-
-    alert("adress of List copied to clipboard: " + tmp.value);
-    document.body.removeChild(tmp);
-
-  };
-
-  return (
-    <div style={{ display: "block", margin: "0 auto", textAlign: "center" }}>
-      <button
-        onClick={handleShareList}
-        // disabled={true}
-        aria-label="Share via Link"
-      >
-        Share List
-      </button>
-      <br />
-      <br />
-
-      <div className="share-button" style={{ textAlign: "center" }}>
-        {/* <div> List-ID: {listId}</div> */}
-
-        <a href={"http://shopping-assistent.herokuapp.com/?listId:" + listId}>
-          {"#" + listId}
-        </a>
-      </div>
-    </div>
-  );
-};
-
-const ShowNewListID = ({ newListId }) => {
-  if (newListId == 0) {
-    return <div></div>;
-  } else {
-    return (
-      <div style={{ display: "block", margin: "0 auto", textAlign: "center" }}>
-        <a
-          href={"http://shopping-assistent.herokuapp.com/?listId:" + newListId}
-        >
-          Open New List
-        </a>
-        <br />
-        <br />
-      </div>
-    );
-  }
 };
 
 export default App;
