d223304ce3a86bc5e972c9197e3fa37955eafe69
Max Upravitelev <max@upravitelev.de>
2020-09-03 16:51:43 +0200

    fix getting list from db

--- src/services/lists.js
+++ src/services/lists.js
@@ -2,10 +2,10 @@ import axios from 'axios'
 
 
 // testing with local backend
-const baseUrl = 'http://localhost:3001/api/lists/'
+// const baseUrl = 'http://localhost:3001/api/lists/'
 
 // testing with remote backend
-// const baseUrl = 'https://shopping-assistant-json-server.herokuapp.com/api/lists/'
+const baseUrl = 'https://shopping-assistant-json-server.herokuapp.com/api/lists/'
 
 // production version 
 // const baseUrl = '/api/lists/'
