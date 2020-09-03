3766265397d33f141a93a46f579785bf2aa36f28
Max Upravitelev <max@upravitelev.de>
2020-09-03 16:16:38 +0200

    rename js files to tsx

---
+++ src/components/Item.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+
+function Item({ item, index, completeItem, removeItem, setColor }) {
+    return (
+      <section
+        className="item"
+
+        //todo: check for screen reader compatibility
+        style={{ textDecoration: item.isCompleted ? "line-through" : "" }}
+      >
+        {item.amount}
+        x
+        {item.text}
+  
+        <section>
+          <button 
+          className="editButtons" style={setColor}
+          onClick={() => completeItem(index)}
+          aria-label='click to mark item as bought'
+  
+          >✓</button>
+  
+          <button 
+          className="editButtons" style={setColor}
+          onClick={() => removeItem(index)}
+          aria-label='remove item from list'
+  
+          >x</button>
+          
+        </section>
+      </section>
+    );
+  }
+
+export default Item;
